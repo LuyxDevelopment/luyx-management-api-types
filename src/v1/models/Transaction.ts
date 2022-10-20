269fafcbd07c52f7a8672d9afbe21b38be43705b
Jaren Goldberg <fyrlex@gmail.com>
2022-10-20 09:12:36 -0400

    refactor: move transactions

--- src/v1/models/Transaction.ts
+++ src/v1/models/Transaction.ts
@@ -1,24 +1,13 @@
+import { TransactionStage, TransactionType } from '../util/transactions.js';
 import { Base } from './base.js';
-
-export enum TransactionStage {
-	WITHDRAWAL_PENDING,
-	DEPOSIT_PENDING,
-	COMPLETED,
-	FAILED,
-}
-
-export enum TransactionType {
-	WITHDRAWAL,
-	DEPOSIT,
-	TRANSFER,
-}
+import { Wallet } from './Wallet.js';
 
 export interface Transaction extends Base {
 	amount: number;
-	transactionType: TransactionType;
-	timestamp: number;
 	description: string;
-	from: null | string;
-	to: null | string;
+	from: null | Wallet;
+	to: null | Wallet;
+	timestamp: number;
 	transactionStage: TransactionStage;
+	transactionType: TransactionType;
 }
