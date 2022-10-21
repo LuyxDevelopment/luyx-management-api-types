132c819d927da55a4951e8320785829975afbfe3
Fyrlex <fyrlex@gmail.com>
2022-10-20 21:32:37 -0400

    remove: unused enum

--- src/v1/models/Wallet.ts
+++ src/v1/models/Wallet.ts
@@ -2,11 +2,6 @@ import { TransactionStage } from '../util/transactions.js';
 import { Base } from './base.js';
 import { Transaction } from './Transaction.js';
 
-export enum TransactionStatus {
-	RECIPIENT_NOT_FOUND,
-	NOT_ENOUGH_FUNDS,
-}
-
 export interface Wallet extends Base {
 	balance: number;
 	deposit(amount: number, description: string): Promise<TransactionStage | Transaction>;
