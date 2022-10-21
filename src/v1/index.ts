e512c56b02499495675b4b8482bea40c0e2cbeca
Fyrlex <fyrlex@gmail.com>
2022-10-20 21:32:22 -0400

    fix missing export

--- src/v1/index.ts
+++ src/v1/index.ts
@@ -1,2 +1,3 @@
 export * from './models/index.js';
 export * from './routes/index.js';
+export * from './util/index.js';
---
+++ src/v1/util/index.ts
@@ -0,0 +1,6 @@
+export * from './country_codes.js';
+export * from './http_codes.js';
+export * from './http_responses.js';
+export * from './job_titles.js';
+export * from './timezones.js';
+export * from './transactions.js';
