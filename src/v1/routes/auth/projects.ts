81f9e0edfc9017687c6164949025891a8351c307
Fyrlex <fyrlex@gmail.com>
2022-10-20 21:54:43 -0400

    fix: assign user return type

--- src/v1/routes/auth/projects.ts
+++ src/v1/routes/auth/projects.ts
@@ -19,14 +19,14 @@ export interface PostProjectRouteOptions extends BaseAuthRouteOptions<Project> {
 	Body: Pick<Project, 'createdAt' | 'deadline' | 'description' | 'gitHubURL' | 'name'>;
 }
 
-export interface DeleteProjectAssignedRouteOptions extends BaseAuthRouteOptions<Project> {
+export interface DeleteProjectAssignedRouteOptions extends BaseAuthRouteOptions<boolean> {
 	Params: Params;
 	Body: {
 		userId: string;
 	};
 }
 
-export interface PutProjectAssignedRouteOptions extends BaseAuthRouteOptions<Project> {
+export interface PutProjectAssignedRouteOptions extends BaseAuthRouteOptions<boolean> {
 	Params: Params;
 	Body: {
 		userId: string;
