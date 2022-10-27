7e2713a60000ca1a804f7a51e7c6f5e5062ff91c
Fyrlex <fyrlex@gmail.com>
2022-10-27 18:59:38 -0400

    fix: transaction stage

--- src/v1/routes/auth/transactions.ts
+++ src/v1/routes/auth/transactions.ts
@@ -1,3 +1,4 @@
+import { TransactionStage } from '../../index.js';
 import { Transaction } from '../../models/Transaction.js';
 import { PartialPick } from '../../util/util.js';
 import { BaseAuthRouteOptions, Params } from '../base.js';
@@ -7,6 +8,6 @@ export interface GetTransactionRouteOptions extends BaseAuthRouteOptions<Transac
 	Querystring: Partial<Pick<Transaction, 'amount' | 'description' | 'from' | 'timestamp' | 'to' | 'transactionStage' | 'transactionType'>>;
 }
 
-export interface PostTransactionRouteOptions extends BaseAuthRouteOptions<Transaction> {
+export interface PostTransactionRouteOptions extends BaseAuthRouteOptions<Transaction | TransactionStage> {
 	Body: PartialPick<Transaction, 'from' | 'to', 'amount' | 'description' | 'transactionStage' | 'transactionType'>;
 }
