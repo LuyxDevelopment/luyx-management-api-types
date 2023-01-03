9b7872349bb167aa27c72aad76d4fe32126e06f4
Jaren Goldberg <fyrlex@gmail.com>
2023-01-03 11:46:46 -0500

    fix: nullify avatar and github

--- src/v1/models/APIUser.ts
+++ src/v1/models/APIUser.ts
@@ -3,14 +3,14 @@ import { Base } from './base.js';
 
 export interface APIUser extends Base {
 	alias: string;
-	avatarURL: string;
+	avatarURL: string | null;
 	firstName: string;
 	lastName: string;
 	isPrivate: boolean;
 	contact: {
 		discordId: string;
 		email: string;
-		gitHub: string;
+		gitHub: string | null;
 	};
 	hiredAt: number;
 	info: {
