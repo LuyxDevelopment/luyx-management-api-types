3029d7d4ab0af22bba5cd589e643c79a5aa8fd3a
Jaren Goldberg <fyrlex@gmail.com>
2023-01-13 22:06:39 -0500

    feat: add consts

--- src/v1/utils/token.ts
+++ src/v1/utils/token.ts
@@ -1,16 +1,31 @@
-export type APIRoute =
-	| '/v1/'
-	| '/v1/projects/'
-	| '/v1/projects/:id'
-	| '/v1/projects/:id/assigned'
-	| '/v1/projects/:id/assigned/:id'
-	| '/v1/token/'
-	| '/v1/transactions/'
-	| '/v1/transactions/:id'
-	| '/v1/users/'
-	| '/v1/users/:id';
+export const APIRouts = [
+	'/v1/',
+	'/v1/projects/',
+	'/v1/projects/:id',
+	'/v1/projects/:id/assigned',
+	'/v1/projects/:id/assigned/:id',
+	'/v1/token/',
+	'/v1/transactions/',
+	'/v1/transactions/:id',
+	'/v1/users/',
+	'/v1/users/:id',
+] as const;
 
-export type APIMethod = 'GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE' | 'CONNECT' | 'OPTIONS' | 'TRACE' | 'PATCH';
+export type APIRoute = typeof APIRouts[number];
+
+export const APIMethods = [
+	'GET',
+	'HEAD',
+	'POST',
+	'PUT',
+	'DELETE',
+	'CONNECT',
+	'OPTIONS',
+	'TRACE',
+	'PATCH',
+] as const;
+
+export type APIMethod = typeof APIMethods[number];
 
 export interface RoutePermission {
 	route: APIRoute;
