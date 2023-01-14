49ae88f824664d863990c13bcb5761813a42d4a8
Jaren Goldberg <fyrlex@gmail.com>
2023-01-13 21:32:55 -0500

    feat: add token permission and user prop

--- src/v1/models/APIToken.ts
+++ src/v1/models/APIToken.ts
@@ -1,7 +1,10 @@
+import { TokenPermission as RoutePermission } from '../utils/token.js';
 import { Base } from './base.js';
 
 export interface APIToken extends Base {
 	token: string;
 	name: string;
 	ip: string;
+	permissions: RoutePermission[];
+	user: string | null;
 }
